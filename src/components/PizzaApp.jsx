87b3de818ec6d7973e6242d7c205e56fe71af584
Maxim Murysov <murisov98@mail.ru>
2026-02-18 10:19:20 +0300

    feat: add pizza cart with add, increment, decrement and disable at min

--- src/components/PizzaApp.jsx
+++ src/components/PizzaApp.jsx
@@ -1,7 +1,63 @@
 import { useState } from "react";
 import styles from "./styles.module.css";
-
+import { pizzas } from "./main";
 function PizzaApp() {
-  return <div>vvv</div>;
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (pizza) => {
+    const newPizza = {
+      id: crypto.randomUUID(),
+      name: pizza.name,
+      price: Number(pizza.price),
+      inCart: false,
+      quantity: 1,
+    };
+    setCart([...cart, newPizza]);
+  };
+  const incrementPizza = (id) => {
+    setCart(
+      cart.map((elem) =>
+        elem.id === id ? { ...elem, quanity: elem.quanity + 1 } : elem,
+      ),
+    );
+  };
+  const decrementPizza = (id) => {
+    setCart(
+      cart.map((elem) =>
+        elem.id === id ? { ...elem, quanity: elem.quanity - 1 } : elem,
+      ),
+    );
+  };
+  return (
+    <div className={styles.pizza}>
+      <div className={styles.pizzaContainer}>
+        <div className={styles.pizzaList}>
+          {pizzas.map((elem) => (
+            <div key={elem.name} className={styles.pizzaItem}>
+              <p className={styles.pizzaName}>{elem.name}</p>
+              <p className={styles.pizzaPrice}>{elem.price}</p>
+              <button onClick={() => addToCart(elem)}>Добавить</button>
+            </div>
+          ))}
+        </div>
+        <div className={styles.cartContainer}>
+          {cart.map((elem) => (
+            <div key={elem.id} className={styles.cartItem}>
+              <p>{elem.name}</p>
+              <button onClick={() => incrementPizza(elem.id)}>+</button>
+              <p>{elem.quantity}</p>
+              <button
+                disabled={elem.quanity <= 1}
+                onClick={() => decrementPizza(elem.id)}
+              >
+                -
+              </button>
+              <p>{elem.price}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 export default PizzaApp;
---
+++ src/components/main.js
@@ -0,0 +1,5 @@
+export const pizzas = [
+  { name: "Маргарита", price: 450 },
+  { name: "4 сыра", price: 550 },
+  { name: "Мясная", price: 650 },
+];
